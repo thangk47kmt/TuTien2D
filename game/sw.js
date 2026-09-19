09c7b3b58c56970a58b482d2889f65f75007faa2
thangk47kmt <40916794+thangk47kmt@users.noreply.github.com>
2026-09-19 09:05:09 -0700

    Add game/sw.js

---
+++ game/sw.js
@@ -0,0 +1,5 @@
+const CACHE='tu-tien-mvp-v1';
+const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match('index.html'))))});
